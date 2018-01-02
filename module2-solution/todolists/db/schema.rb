2feb541de7e2cd911a3808369cffd8a9b392bec6
Joel Sequeira <joen.sg@gmail.com>
2018-01-02 10:50:20 +0000

    finish rq05

--- module2-solution/todolists/db/schema.rb
+++ module2-solution/todolists/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102102441) do
+ActiveRecord::Schema.define(version: 20180102104638) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 20180102102441) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
+
+  create_table "todo_lists_users", force: :cascade do |t|
+    t.integer "user_id"
+  end
+
+  add_index "todo_lists_users", ["user_id"], name: "index_todo_lists_users_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
