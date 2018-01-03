9f379ad158e139cbb97e90b724be9d914519ee09
Joel Sequeira <joen.sg@gmail.com>
2018-01-03 10:41:26 +0000

    finish rq11

--- module2-solution/todolists/app/models/profile.rb
+++ module2-solution/todolists/app/models/profile.rb
@@ -1,3 +1,19 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validate :first_name_and_last_name_not_null
+  validate :male_first_name_not_sue
+  validates :gender, :inclusion => %w(male female)
+
+  def first_name_and_last_name_not_null
+    if first_name.nil? && last_name.nil?
+      errors.add(:first_name, "first_name or last_name can be null but not both")
+    end
+  end
+
+  def male_first_name_not_sue
+    if gender == "male" && first_name == "Sue"
+      errors.add(:first_name, "male with first_name Sue not allowed")
+    end
+  end
 end
