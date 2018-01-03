040fbedab36ceddeef4ab9e81816506610b5ef47
Joel Sequeira <joen.sg@gmail.com>
2018-01-03 13:37:21 +0000

    finish rq14

--- module2-solution/todolists/app/models/profile.rb
+++ module2-solution/todolists/app/models/profile.rb
@@ -16,4 +16,8 @@ class Profile < ActiveRecord::Base
       errors.add(:first_name, "male with first_name Sue not allowed")
     end
   end
+
+  def self.get_all_profiles min_year, max_year
+    self.where("birth_year BETWEEN :min_year AND :max_year", min_year: min_year, max_year: max_year).order(:birth_year)
+  end
 end
