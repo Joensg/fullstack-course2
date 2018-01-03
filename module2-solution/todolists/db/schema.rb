d9d400d0350916de61f46378480eddaf7275264d
Joel Sequeira <joen.sg@gmail.com>
2018-01-03 08:46:38 +0000

    fix migration in associating todoLists to users.

--- module2-solution/todolists/db/schema.rb
+++ module2-solution/todolists/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102105532) do
+ActiveRecord::Schema.define(version: 20180103084441) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -42,13 +42,10 @@ ActiveRecord::Schema.define(version: 20180102105532) do
     t.date     "list_due_date"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
   end
 
-  create_table "todo_lists_users", force: :cascade do |t|
-    t.integer "user_id"
-  end
-
-  add_index "todo_lists_users", ["user_id"], name: "index_todo_lists_users_on_user_id"
+  add_index "todo_lists", ["user_id"], name: "index_todo_lists_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
